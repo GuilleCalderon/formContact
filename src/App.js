e4c722f9025a908b8bad8941b9543a1ec70526fb
GuilleCalderon <guillermo.calde46@gmail.com>
2022-02-24 16:31:43 -0300

    modales

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import ContactForm from './components/ContactForm';
+import "./App.css";
+import ContactForm from "./components/ContactForm";
+import Modals from "./components/Modals";
 
 function App() {
   return (
     <>
       <h2>Ejercitacion React</h2>
 
+      <hr />
+      <Modals />
+      <hr />
       <ContactForm />
-      
     </>
   );
 }
---
+++ src/components/Modals.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useModal } from '../hooks/useModal'
+import ContactForm from './ContactForm'
+import Modal from './Modal'
+
+
+const Modals = () => {
+
+  const [isOpenModal1, openModal1 ,closeModal1] = useModal(false)
+  const [isOpenModal2, openModal2 ,closeModal2] = useModal(false)
+  const [isOpenContact, openModalContact, closeModaleContact] = useModal(false)
+
+  return (
+    <div>
+      <h2>Modales!</h2>
+      <button onClick={openModal1} >Modal 1</button>
+
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
+        <h3>Modal 1</h3>
+        <p>Hola este es el contenido de mi modal 1</p>
+        <img src="https://placeimg.com/400/400/animals" alt="Animals" />
+      </Modal>
+
+      <button onClick={openModal2} >Modal 2</button>
+
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
+        <h3>Otro modal</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit blanditiis ad, optio similique, quidem eum sint ex nulla quis itaque laudantium ullam enim velit harum eligendi deserunt fugiat. Sunt, quae?</p>
+        <img src="https://placeimg.com/400/400/nature" alt="Nature" />
+
+      </Modal>
+
+      <button onClick={openModalContact} >Modal Contacto</button>
+      <Modal isOpen={isOpenContact} closeModal={closeModaleContact}>
+        <ContactForm />
+      </Modal>
+
+    </div>
+  )
+}
+
+export default Modals
